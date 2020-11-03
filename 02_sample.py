65f39b03480cb0d9e4b7ad70518322c69bcb7e19
carlo <carlo.vanderstoep@gmail.com>
2020-11-03 17:25:08 +0100

    added additional files to track

---
+++ 02_sample.py
@@ -0,0 +1,8 @@
+LetterNum = 1
+
+for Letter in "Groet!":
+	if Letter == "o":
+		pass #do niets statement
+		print("stuitte op o, niet verwerkt.")
+	print("Letter ", LetterNum, " is ", Letter)
+	LetterNum +=1
